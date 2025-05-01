a66b9161773b2f70ffd24b3dc8336dce31f31d80
innocent peter <otakhorpeter@gmail.com>
2025-05-01 23:09:13 +0100

    fix for food id page

--- apps/web/app/(root)/user/food/[id]/page.tsx
+++ apps/web/app/(root)/user/food/[id]/page.tsx
@@ -1,22 +1,13 @@
 // app/(root)/user/food/[id]/page.tsx
 
-import { type Metadata } from "next";
 import FoodDetailClient from "@/app/(root)/user/food/[id]/FoodDetailClient";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function FoodDetailPage({ params }: PageProps) {
-  return <FoodDetailClient id={params.id} />;
-}
-export async function generateMetadata({
+export default async function FoodDetailPage({
   params,
-}: PageProps): Promise<Metadata> {
-  return {
-    title: `Food Item - ${params.id}`,
-  };
-}
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const foodId = (await params).id;
 
+  return <FoodDetailClient id={foodId} />;
+}
