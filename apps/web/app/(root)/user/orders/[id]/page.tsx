b429b9574a4562fb9a806962097767ab6a13ba34
innocent peter <otakhorpeter@gmail.com>
2025-06-05 21:47:16 +0100

    test 2

--- apps/web/app/(root)/user/orders/[id]/page.tsx
+++ apps/web/app/(root)/user/orders/[id]/page.tsx
@@ -69,7 +69,7 @@ export default async function OrderDetailPage({
   const cookieStore = await cookies();
   const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
         async getAll() {
